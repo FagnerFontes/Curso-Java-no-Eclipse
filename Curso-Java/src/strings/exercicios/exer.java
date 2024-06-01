c5b274b861bc2de8d8362520ff8253da0dc6cb20
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2024-05-31 22:41:55 -0300

    Final de Exercicicios de Strings

--- Curso-Java/src/strings/exercicios/exer.java
+++ Curso-Java/src/strings/exercicios/exer.java
@@ -1,16 +1,20 @@
 package strings.exercicios;
-import java.util.Scanner; 
+
+import java.util.Scanner;
+
 public class exer {
 
     public static void main(String[] args) {
-        
+
         Scanner porta = new Scanner(System.in);
 
-       String nome = " Fagner"; 
+        //Declara uma String
+        String nome = "Fagner Fontes";
 
-       String replaced = nome.replace("Fagner", "Esmeralda"); 
-       
-       System.out.println(replaced);
-       
+        //Verifica se na String tem um determinado caracter
+        boolean verifica = nome.startsWith("F");
+
+    //Imprime o resultado da busca
+      System.out.println(verifica);
     }
 }
